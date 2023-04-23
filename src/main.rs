87c41e9580a0eff9a91c2a64a261d2bf8e30ee7c
jaymody <jaykmody@gmail.com>
2023-04-23 18:18:54 -0400

    Finished part 1.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        // prompt
+        print!("\ndb > ");
+        _ = io::stdout().flush();
+
+        // get input str
+        let mut input_buffer = String::new();
+        io::stdin()
+            .read_line(&mut input_buffer)
+            .expect("Failed to read line");
+        let input_str = input_buffer.as_str().trim();
+
+        // response
+        match input_str {
+            ".exit" => break,
+            _ => println!("Unrecognized command: {}", input_str),
+        }
+    }
 }
