8bbcc398353bd98218ae52a381a6074a9bbecab2
jaymody <jaykmody@gmail.com>
2023-04-25 21:02:47 -0400

    Basic test suite for repl.

--- src/repl.rs
+++ src/repl.rs
@@ -4,6 +4,8 @@ use std::str::FromStr;
 use crate::db::row::Row;
 use crate::db::table::Table;
 
+const PROMPT: &str = "\n\x1b[01;32mrustdb >\x1b[00m ";
+
 pub fn start<R, W>(mut reader: R, mut writer: W)
 where
     R: std::io::BufRead,
@@ -13,7 +15,7 @@ where
 
     loop {
         // prompt
-        write!(writer, "\n{}rustdb >{} ", "\x1b[01;32m", "\x1b[00m").unwrap();
+        write!(writer, "{}", PROMPT).unwrap();
         writer.flush().unwrap();
 
         // get input str
@@ -28,7 +30,7 @@ where
                     if let Err(msg) = table.close() {
                         writeln!(writer, "{}", msg).unwrap();
                     }
-                    std::process::exit(0)
+                    return;
                 }
                 _ => {
                     writeln!(writer, "Unrecognized command: {}", input).unwrap();
@@ -54,5 +56,56 @@ where
         } else {
             writeln!(writer, "Unrecognized keyword: {}", input).unwrap();
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::vec;
+
+    use super::*;
+
+    fn run_lines(input_lines: &Vec<String>) -> Vec<String> {
+        let input = input_lines.join("\n");
+        let input = input.as_bytes();
+
+        let mut output = Vec::new();
+        start(&input[..], &mut output);
+
+        let output_str = String::from_utf8(output).unwrap();
+        let mut output_lines: Vec<&str> = output_str.split(&format!("{}", PROMPT)).collect();
+
+        // get rid of anything that gets printed before first prompt is printed
+        // then check that there is exactly 1 output line per input line
+        output_lines.remove(0);
+        assert_eq!(input_lines.len(), output_lines.len());
+
+        // remove any trailing whitespace and convert to owned String
+        let output_lines: Vec<String> = output_lines
+            .iter()
+            .map(|s| s.trim_end().to_string())
+            .collect();
+
+        output_lines
+    }
+
+    fn check_input_output_pairs(pairs: Vec<(&str, &str)>) {
+        let (input_lines, expected_output): (Vec<String>, Vec<String>) = pairs
+            .into_iter()
+            .map(|x| (x.0.to_owned(), x.1.to_owned()))
+            .unzip();
+
+        let actual_output = run_lines(&input_lines);
+
+        assert_eq!(expected_output, actual_output);
+    }
+
+    #[test]
+    fn insert_and_retrieve_row() {
+        check_input_output_pairs(vec![
+            ("insert 1 user1 person1@example.com", "Executed."),
+            ("select", "(1, user1, person1@example.com)\n\nExecuted."),
+            (".exit", ""),
+        ]);
     }
 }
